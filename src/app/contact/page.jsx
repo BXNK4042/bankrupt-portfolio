d71bfb07ba93d91c15a28bea6183109701d14986
BXNK4042 <parinthonkongthong@gmail.com>
2025-11-05 15:39:27 +0700

    Portfolio V.1

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
+import Mail from '../../../public/gmail.svg'
+import Github from '../../../public/github.svg'
+import LinkedIn from '../../../public/linkedin.svg'
 
 function page() {
   return (
-    <div>
+    <div className='mx-50 my-30'>
+      <div className='flex w-full h-150'>
+        <div className='bg-blue-100 w-1/2'>
+          
+        </div>
+        <div className='bg-blue-50/75 w-3/2'>
+          <div className='m-10'>
+            <h1 className='text-4xl'>Contact</h1>
+            <div className='mt-15 space-y-1'>
+              <div className='flex items-center'>
+                <Image src={Mail} width={50} height={50} alt='mail-icon'></Image>
+                <h1 className='ml-5 text-lg'>parinthonkongthong@gmail.com</h1> 
+              </div>
+              <br />
+              <div className='flex items-center'>
+                <Image src={Github} width={50} height={50} alt='github-icon'></Image>
+                <h1 className='ml-5 text-lg'>BXNK4042</h1>
+              </div>
+              <br />
+              <div className='flex items-center'>
+                 <Image src={LinkedIn} width={50} height={50} alt='linkedin-icon'></Image>
+                 <h1 className='ml-5 text-lg'>(Working on it)</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,7 +7,7 @@ function page() {
     <div>
       <div className='mx-50 my-15'>
         <div className=''>
-          <h1>Projects</h1>
+          <h1 className='text-2xl'>Projects</h1>
           <div className='m-10'>
             <ul className='grid grid-cols-4 gap-y-15'>
               <li className='shadow-md rounded-xl w-50 h-50 bg-blue-50'>
@@ -22,13 +22,23 @@ function page() {
               </li>
             </ul>
           </div>
-          <h1>Certificate</h1>
+          <h1 className='text-2xl'>Certificate</h1>
           <div className='m-10'>
             <ul className='grid grid-cols-4 gap-y-15'>
               <li className='shadow-md rounded-xl w-50 h-50 bg-blue-50'>
                 <div>
                   <div className='flex justify-center m-5'>
                     <h1>CTF2025</h1>
+                  </div>
+                  <div className='flex justify-center m-5'>
+                    <Image src={notFound} alt="not_found" width={100} height={100}/>
+                  </div>
+                </div>
+              </li>
+              <li className='shadow-md rounded-xl w-50 h-50 bg-blue-50'>
+                <div>
+                  <div className='flex justify-center m-5'>
+                    <h1>KMITL LEX 2025</h1>
                   </div>
                   <div className='flex justify-center m-5'>
                     <Image src={notFound} alt="not_found" width={100} height={100}/>
